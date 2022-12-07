685a6f3a11a56f37bf91ffe54d7324c56f031268
Prince Muel <vansomecsam@gmail.com>
2022-12-07 01:29:25 +0100

    ci: add gh-pages for site build

--- src/hooks/use-numOfPlayers.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export function useNumOfPlayers(numOfPlayers: number) {
-  const [numOfPlayers, setNumOfPlayers] = useState(numOfPlayers);
-  return { numOfPlayers, setNumOfPlayers};
-}
-
-export type UseNumOfPlayersType = ReturnType<typeof useNumOfPlayers>

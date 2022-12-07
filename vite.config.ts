186acda0b03583f7160691a6a90deba3bea8626f
Prince Muel <vansomecsam@gmail.com>
2022-12-07 02:41:23 +0100

    ci: add gh-pages and configure build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-// @ts-nocheck
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/memory-game/', // base url of the project's site
-  plugins: [
-    react(),
-    ghPages({
-      branch: 'docs',
-      dest: '/dist',
-      user: {
-        name: 'Prince Muel',
-        email: 'vasnsomecsam@gmail.com',
-      },
-    }),
-  ],
+  plugins: [react()],
 });

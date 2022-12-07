94f9bd7f32d8312ad50391c47b8ef3b7d6eef7aa
Prince Muel <vansomecsam@gmail.com>
2022-12-07 02:34:04 +0100

    ci: add changes to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { ghPages } from 'vite-plugin-gh-pages';
@@ -5,5 +6,14 @@ import { ghPages } from 'vite-plugin-gh-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/memory-game/', // base url of the project's site
-  plugins: [react(), ghPages()],
+  plugins: [
+    react(),
+    ghPages({
+      dest: '/dist',
+      user: {
+        name: 'Prince Muel',
+        email: 'vasnsomecsam@gmail.com',
+      },
+    }),
+  ],
 });

4006a162646d67eb5b98ac8cf58aef086503b4f9
Prince Muel <vansomecsam@gmail.com>
2022-12-07 02:04:51 +0100

    ci: update gh-pages deployment config

---
+++ notes.md
@@ -0,0 +1,8 @@
+# Notes
+
+```json
+{
+  "predeploy": "yarn run build",
+  "deploy": "gh-pages -d dist"
+}
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/memory-game/', // base url of the project's site
-  plugins: [react()],
+  plugins: [react(), ghPages()],
 });

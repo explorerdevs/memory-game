3239301cc86b386a4a46f2afa96b780104e983eb
Prince Muel <vansomecsam@gmail.com>
2022-12-20 04:12:00 +0100

    ci: update tailwind configuration
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,45 +1,46 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const plugin = require("tailwindcss/plugin");
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     borderRadius: {
       ...defaultTheme.borderRadius,
-      pill: "100vmax",
+      pill: '100vmax',
     },
 
     screens: {
-      xs: "30em", // => @media (min-width: 480px) { ... }
+      xs: '30em', // => @media (min-width: 480px) { ... }
       ...defaultTheme.screens,
     },
 
     extend: {
       screens: {
-        sm: "40em", // => @media (min-width: 640px) { ... }
-        md: "48em", // => @media (min-width: 768px) { ... }
-        lg: "64em", // => @media (min-width: 1024px) { ... }
-        xl: "80em", // => @media (min-width: 1280px) { ... }
-        "2xl": "96em", // => @media (min-width: 1536px) { ... }
+        sm: '40em', // => @media (min-width: 640px) { ... }
+        md: '48em', // => @media (min-width: 768px) { ... }
+        lg: '64em', // => @media (min-width: 1024px) { ... }
+        xl: '80em', // => @media (min-width: 1280px) { ... }
+        '2xl': '96em', // => @media (min-width: 1536px) { ... }
       },
       colors: {
         primary: {
-          100: "#7ec384",
-          200: "#3fc384",
-          300: "#45625d",
+          100: '#7ec384',
+          200: '#68bf8c',
+          300: '#3fc384',
         },
         secondary: {
-          100: "#a3d2d5",
-          200: "#6aa5a9",
+          100: '#a3d2d5',
+          200: '#6aa5a9',
+          300: '#45625d',
         },
       },
       gridTemplateColumns: {
         // arbitrary values
-        "fill-16": "repeat(auto-fill, minmax(4rem, 1fr))",
-        "fill-20": "repeat(auto-fill, minmax(5rem, 1fr))",
-        "fit-big": "repeat(auto-fit, minmax(25rem, 1fr))",
+        'fill-16': 'repeat(auto-fill, minmax(4rem, 1fr))',
+        'fill-20': 'repeat(auto-fill, minmax(5rem, 1fr))',
+        'fit-big': 'repeat(auto-fit, minmax(25rem, 1fr))',
         // etc.
       },
     },
@@ -47,19 +48,19 @@ module.exports = {
   plugins: [
     plugin(function ({ addUtilities }) {
       addUtilities({
-        ".svg-icon": {
-          fill: "currentColor",
+        '.svg-icon': {
+          fill: 'currentColor',
         },
-        ".w-full-shadow": {
-          boxShadow: "0 0 0 100vmax currentColor, 0 0 2rem currentColor",
-          clipPath: "inset(0 -100vmax)",
+        '.w-full-shadow': {
+          boxShadow: '0 0 0 100vmax currentColor, 0 0 2rem currentColor',
+          clipPath: 'inset(0 -100vmax)',
         },
-        ".h-container": {
-          "--max-width": "111rem",
-          "--container-padding": "1.6rem",
+        '.h-container': {
+          '--max-width': '64rem',
+          '--container-padding': '2rem',
 
-          width: "min(var(--max-width), 100% - (var(--container-padding) * 2))",
-          marginInline: "auto",
+          width: 'min(var(--max-width), 100% - (var(--container-padding) * 2))',
+          marginInline: 'auto',
         },
       });
     }),

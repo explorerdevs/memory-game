5caca6cb24c66c25d632065d2c7cf2e651713ab2
Ash Moreno <ashterfuentes@gmail.com>
2022-12-20 16:59:44 -0600

    feat(ui): style header and footer

--- src/components/atoms/theme-toggle.tsx
+++ src/components/atoms/theme-toggle.tsx
@@ -1,15 +1,20 @@
+import { clsx } from '../../common';
 import { useSetTheme, useTheme } from '../../context';
 
-type Props = {};
+type Props = { classname: string };
 
 const ThemeToggle = (props: Props) => {
   const setTheme = useSetTheme();
   const theme = useTheme();
+  const classes = clsx(
+    'inline-grid aspect-square w-12 place-content-center rounded-full bg-secondary-300',
+    props.classname && props.classname
+  );
   return (
     <button
       type="button"
       title="Toggle Theme"
-      className="inline-grid aspect-square w-12 place-content-center rounded-full bg-secondary-300"
+      className={classes}
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       <span>🤭</span>
